deae483401f1c7041106c7a709a7b015aa6adae7
callejas89 <estebancallejas89@outlook.es>
2021-11-04 07:34:03 -0500

    front

---
+++ Frontend/js/funcionesCategoria.js
@@ -0,0 +1,121 @@
+function traerInformacionCategoria(){
+    console.log("test");
+        $.ajax({
+        url:"http://129.151.108.112:8081/api/Category/all",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuesta(respuesta);
+        }
+    });
+}
+
+function pintarRespuesta(respuesta){
+
+    let myTable="<table>";
+    for(i=0;i<respuesta.length;i++){
+        myTable+="<tr>";
+        myTable+="<td>"+respuesta[i].name+"</td>";
+        myTable+="<td>"+respuesta[i].description+"</td>";
+        myTable+="<td> <button onclick=' actualizarInformacionCategoria("+respuesta[i].id+")'>Actualizar</button>";
+        myTable+="<td> <button onclick='borrarCategoria("+respuesta[i].id+")'>Borrar</button>";
+        myTable+="</tr>";
+    }
+    myTable+="</table>";
+    $("#resultado1").html(myTable);
+}
+
+function guardarInformacionCategoria(){
+   
+    if ($("#Categoryname").val().length==0 || $("#Categorydescription").val().length==0){
+
+        alert("Todos los campos son obligatorios");
+    }else{
+   
+    let var2 = {
+        name:$("#Categoryname").val(),
+        description:$("#Categorydescription").val()
+        };
+      
+        $.ajax({
+        type:'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'JSON',
+        data: JSON.stringify(var2),
+        
+        url:"http://129.151.108.112:8081/api/Category/save",
+       
+        
+        success:function(response) {
+                console.log(response);
+            console.log("Se guardo correctamente");
+            alert("Se guardo correctamente la nueva categoria");
+            window.location.reload()
+    
+        },
+        
+        error: function(jqXHR, textStatus, errorThrown) {
+              window.location.reload()
+            alert("No se guardo correctamente la nueva categoria");
+    
+    
+        }
+        });}
+
+}
+
+function actualizarInformacionCategoria(idElemento){
+    
+    if ($("#Categoryname").val().length==0 || $("#Categorydescription").val().length==0){
+
+        alert("Todos los campos son obligatorios");
+    }else{
+    
+    
+    let myData={
+        id:idElemento,
+        name:$("#Categoryname").val(),
+        description:$("#Categorydescription").val()
+
+    };
+    console.log(myData);
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"http://129.151.108.112:8081/api/Category/update",
+        type:"PUT",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            $("#id").val("");
+            $("#Categoryname").val("");
+            $("#Categorydescription").val("");
+            traerInformacionCategoria();
+            alert("se ha Actualizado correctamente la categoria")
+        }
+    });}
+
+}
+
+function borrarCategoria(idElemento){
+    let myData={
+        id:idElemento
+    };
+    let dataToSend=JSON.stringify(myData);
+    console.log(dataToSend);
+    $.ajax({
+        url:"http://129.151.108.112:8081/api/Category/"+idElemento,
+        type:"DELETE",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            traerInformacionCategoria();
+            alert("Se ha Eliminado correctamente la categoria.")
+        }
+    });
+
+}
